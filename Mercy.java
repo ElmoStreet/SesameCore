4bb0d0f2e756c3f998d747522167f744b8122165
ElmoStreet <agarlogan@gmail.com>
2017-09-19 09:16:39 -0400

    Create Mercy.java

---
+++ Mercy.java
@@ -0,0 +1,19 @@
+import bukkit.org.*
+import java.util
+
+public static String getHero(Player p){
+if(HeroManager.hero.get(p) == null){
+ return null;
+}else{
+ return HeroManager.hero.get(p);
+}
+}
+
+public static void equip(Player p){
+  if(getHero(p) == null){
+   p.sendMessage(Messages.format("&cFailed to equip");
+  }else{
+  PlayerInventory pi = p.getInventory();
+  
+  }
+}
