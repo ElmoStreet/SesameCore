63a719f8549907238d3f901414a5ce2b520a9dcd
ElmoStreet <agarlogan@gmail.com>
2017-09-19 12:09:16 -0400

    Update HeroManager.java

--- HeroManager.java
+++ HeroManager.java
@@ -1,6 +1,10 @@
-import bukkit.org.*
-import *
+import bukkit.org.*;
+import *;
 
 public class HeroManager {
-
+    public static HashMap<Player,String> hero = new HashMap<Player,String>();
+  
+  public static void setHero(Player p, String hero){
+    hero.put(p,hero);
+  }
 }
